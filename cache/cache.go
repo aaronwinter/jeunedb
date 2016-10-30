b3dc063de68e5e5a3fb99931f977b03de5177af2
Aaron <aaron.ounn@gmail.com>
2016-10-30 17:26:42 -0400

    Create cache constructor and change map signature

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,14 @@
 package cache
 
 type Cache struct {
-	c    map[string][]byte
+	c    map[byte][]byte
 	size uint64
+}
+
+func New() Cache {
+	cache := Cache{
+		c:    make(map[byte][]byte, 0),
+		size: 0,
+	}
+	return cache
 }
