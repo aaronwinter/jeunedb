d78ff360717bf1ea2234b8f5ba92772862c0463e
Aaron <aaron.ounn@gmail.com>
2016-10-31 18:56:27 -0400

    Create Block constructor

--- storage/storage.go
+++ storage/storage.go
@@ -6,3 +6,15 @@ type Block struct {
 	Value  []byte
 	ValueL uint32
 }
+
+func NewBlock(k []byte, v []byte) *Block {
+	lenK := uint32(len(k))
+	lenV := uint32(len(v))
+	b := &Block{
+		Key:    k,
+		KeyL:   lenK,
+		Value:  v,
+		ValueL: lenV,
+	}
+	return b
+}
